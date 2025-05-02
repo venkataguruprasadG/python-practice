9bec903b9f6ecbbdd8496c847ffa6e78a12c03ca
venkataguruprasadG <venkatagp27@gmail.com>
2025-05-02 20:36:04 +0530

    problem 2

---
+++ variables_math.py
@@ -0,0 +1,18 @@
+#Problem 2: Variables & Math
+#Problem Statement
+#Write a Python program that:
+
+#Asks the user to enter two numbers.
+
+#Calculates their sum.
+
+#Prints the result in the format:
+#Sum of X and Y is Z
+#(where X and Y are the numbers entered, and Z is their sum)
+
+num1 = int(input("Enter the number 1: "))
+num2 = int(input("Enter the number 2: "))
+
+Sum = num1 + num2
+
+print(f"Sum of {num1} and {num2} is {Sum}")
