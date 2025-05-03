5e19786cc50603258077a935f51a0241f407ace9
venkataguruprasadG <venkatagp27@gmail.com>
2025-05-03 21:01:04 +0530

    problem 7

---
+++ DAY-3/reverse_digits.py
@@ -0,0 +1,12 @@
+'''
+Problem 7: Reverse Digits
+Write a Python program that takes a positive integer n and prints its digits in reverse order (on one line).
+
+Test Case:
+Input: 12345 → Output: 54321
+'''
+
+n=int(int(input("Enter the number: ")))
+
+print(str(n)[::-1])
+
