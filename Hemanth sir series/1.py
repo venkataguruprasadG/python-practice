51edd6b52fead765f328699ef0549f9c2c9e988a
venkataguruprasadG <venkatagp27@gmail.com>
2025-05-26 15:53:35 +0530

    Hemanth sir coding challenge 1

---
+++ Hemanth sir series/1.py
@@ -0,0 +1,12 @@
+'''
+We will write a simple program that asks for your name, age, and favourite hobby, then 
+prints a fun personalized message.
+'''
+
+name = input("Enter your name: ")
+
+age = int(input("How old are you: "))
+
+hobby = input("What's your favourite hobby?")
+
+print(f"Hey {name}! So you're {age} years old and love {hobby}. That's awesome")
