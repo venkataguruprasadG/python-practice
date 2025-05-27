42fc7d964f996f6ee2756fcc3d2cdaee18f8905d
venkataguruprasadG <venkatagp27@gmail.com>
2025-05-27 12:08:13 +0530

    Input and output

---
+++ python course series.py/1.7.py
@@ -0,0 +1,13 @@
+'''
+Problem 7: Input and Output
+Task:
+Take the user's name as input and print:
+
+text
+Hello, <name>!
+where <name> is replaced by the input.
+'''
+
+name = input("Enter your name here: ")
+
+print(f"Hello: {name}")
