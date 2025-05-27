307030d77de01a7946dd0b0d0e53f3c4afb770e5
venkataguruprasadG <venkatagp27@gmail.com>
2025-05-27 15:35:06 +0530

    String Concatination

---
+++ python course series.py/1.9.py
@@ -0,0 +1,13 @@
+'''
+Problem 9: String Concatenation
+Task:
+Assign your first name and last name to two variables. Print your full name by combining them with a space in between.
+'''
+
+first_name = "Python "
+
+last_name = "Prograamming"
+
+string_join = first_name + last_name
+
+print(string_join)
