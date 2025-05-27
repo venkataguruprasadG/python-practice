692803298696344f87b2d94d9c4cb8d56590a754
venkataguruprasadG <venkatagp27@gmail.com>
2025-05-27 15:39:28 +0530

    just formatting

---
+++ python course series.py/Variables/1.0.py
@@ -0,0 +1,15 @@
+'''
+Problem 10: Variable Reassignment
+Task:
+Assign the value 5 to a variable named x.
+Then, reassign x to the string "Python".
+Print the variable after each assignment.
+'''
+
+x = 5
+
+print(x)
+
+x = "Python"
+
+print(x)
