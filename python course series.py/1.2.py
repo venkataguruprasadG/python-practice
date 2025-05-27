7213c01be5d54a5085db8539883401673b5d0aa8
venkataguruprasadG <venkatagp27@gmail.com>
2025-05-27 11:48:50 +0530

    swap variables

---
+++ python course series.py/1.2.py
@@ -0,0 +1,26 @@
+'''
+Problem 2: Swap Variables
+Task:
+Given two variables x = 5 and y = 8, swap their values and print the result.
+'''
+x = 5
+y = 8
+
+temp = x
+x = y
+y = temp
+
+print(x)
+print(y)
+
+'''
+Another method 
+
+x = 5
+y = 8
+
+x, y = y, x
+
+print(x)
+print(y)
+'''
