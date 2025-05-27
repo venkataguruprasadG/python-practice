045101372df8f557fd441b1c6ed8224d1b70caae
venkataguruprasadG <venkatagp27@gmail.com>
2025-05-27 11:51:05 +0530

    multiple assingment

---
+++ python course series.py/1.3.py
@@ -0,0 +1,11 @@
+'''
+Problem 3: Multiple Assignment
+Task:
+Assign the values 1, 2, and 3 to variables a, b, and c in a single line. Print all three variables.
+'''
+
+a, b, c = 1, 2, 3
+
+print(a)
+print(b)
+print(c)
