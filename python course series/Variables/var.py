df6bdef53c6a3126a08c37ecd3142e2eef761e38
venkataguruprasadG <venkatagp27@gmail.com>
2025-06-13 21:21:43 +0530

    formatted the file

---
+++ python course series/Variables/var.py
@@ -0,0 +1,12 @@
+'''
+1.	Declare a variable city and assign your hometown to it.
+2.	Declare a variable years_of_experience and set it to the number of years you've been coding.
+3.	Print both variables
+'''
+
+city = 'INDIA'
+years_of_experience = 1
+
+print(city)
+
+print(years_of_experience)
