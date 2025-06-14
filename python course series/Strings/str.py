3cb53fb767ed0bdbad3448fd6d165b38551d45e2
venkataguruprasadG <venkatagp27@gmail.com>
2025-06-14 05:54:01 +0530

    strings

---
+++ python course series/Strings/str.py
@@ -0,0 +1,16 @@
+'''
+1.	Declare a string variable sentence with any phrase you like.
+2.	Print its length using len(sentence).
+3.	Convert it to uppercase using .upper().
+4.	Extract the first 5 characters using slicing.
+5.	Reverse the string
+'''
+
+name = "Guru Prasad"
+print(f"Length of the variable string (name): {len(name)}")
+
+print("converting the (name) variable to uppercase: ",name.upper())
+
+print("Slicing of first 5 characters: ",name[0:5])
+
+print("Reverse of the string: ",name[::-1])
